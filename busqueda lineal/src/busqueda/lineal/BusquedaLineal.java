8bd606474462cab13590cebc650e604459d24f91
Esteban <61157043+Esteebaan23@users.noreply.github.com>
2020-04-10 16:27:29 -0500

    Add files via upload

---
+++ busqueda lineal/src/busqueda/lineal/BusquedaLineal.java
@@ -0,0 +1,14 @@
+
+package busqueda.lineal;
+
+
+public class BusquedaLineal {
+
+
+    public static void main(String[] args) {
+      
+         FrmBusqueda FrmJuego= new FrmBusqueda();
+        FrmJuego.setVisible(true);
+    }
+    
+}
